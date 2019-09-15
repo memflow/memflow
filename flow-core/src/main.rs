8dce7f9d1f5d53300cc004fd53ccfc35ddbb71de
ko1N <ko1N1337@gmail.com>
2019-09-15 20:09:04 +0200

    initial commit

---
+++ flow-core/src/main.rs
@@ -0,0 +1,18 @@
+use clap::{App, Arg};
+use std::time::Instant;
+
+fn main() {
+    let argv = App::new("flow-core")
+        .version("0.1")
+        .arg(
+            Arg::with_name("socket")
+                .short("s")
+                .long("socket")
+                .value_name("FILE")
+                .help("bridge unix socket file")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    // TODO: implement me
+}

cc9512f14d7097ead293d480e34a4f5bcef41edd
ko1N <ko1N1337@gmail.com>
2019-09-24 22:38:41 +0200

    added dtb pseudocode

--- flow-core/src/cpu.rs
+++ flow-core/src/cpu.rs
@@ -3,7 +3,7 @@ use std::io::Result;
 // TODO: implement different traits or whatever for diff archs
 pub enum Architecture {
     X64,
-    X64Pae,
+    X86Pae,
     X86
 }
 
--- flow-win32/src/dtb.rs
+++ flow-win32/src/dtb.rs
@@ -15,28 +15,32 @@ pub struct Windows {
 }
 
 pub fn find(machine: &mut Machine) -> Result<DTB> {
-    let low16 = machine.mem.read_physical_memory(0, 0x100000)?;
-
-    // TODO: prints
     // println!("found x64 dtb in lowstub < 1M: va={:x} dtb={:x}", va, dtb);
 
+    // read low 1mb stub
+    let low1m = machine.mem.read_physical_memory(0, 0x100000)?;
+
     // find x64 dtb in low stub < 1M
-    match find_x64_lowstub(&low16) {
+    match find_x64_lowstub(&low1m) {
         Ok(d) => return Ok(d),
         Err(e) => println!("Error: {}", e),
     }
 
-    match find_x64(&low16) {
+    // TODO: append instead of read twice?
+    // read low 16mb stub
+    let low16m = machine.mem.read_physical_memory(0, 0x1000000)?;
+
+    match find_x64(&low16m) {
         Ok(d) => return Ok(d),
         Err(e) => println!("Error: {}", e),
     }
 
-    match find_x64_pae(&low16) {
+    match find_x86_pae(&low16m) {
         Ok(d) => return Ok(d),
         Err(e) => println!("Error: {}", e),
     }
 
-    match find_x86(&low16) {
+    match find_x86(&low16m) {
         Ok(d) => return Ok(d),
         Err(e) => println!("Error: {}", e),
     }
@@ -61,14 +65,107 @@ pub fn find_x64_lowstub(stub: &Vec<u8>) -> Result<DTB> {
         })
 }
 
-pub fn find_x64(mem: &Vec<u8>) -> Result<DTB> {
-    Err(Error::new(ErrorKind::Other, "unable to find x64 dtb in lowstub < 16M"))
+/*
+* Check if a page looks like the Windows Kernel x86 Directory Table Base (DTB)
+* in the 32-bit PAE memory mode - i.e. the PDPT of the System process.
+* Also please note that this may not be the actual PDPT used by the kernel -
+* it may very well rather be the PDPT probably set up by WinLoad and then the
+* 'System' process uses another. But it works for auto-detect!
+* 1: (4) valid PDPT entries with consecutive physical addresses of the PDPT.
+* 2: all zeroes for the rest of the page.
+*/
+fn _find_x64(mem: &[u8]) -> Option<()> {
+    /*
+    DWORD c, i;
+    BOOL fSelfRef = FALSE;
+    QWORD pte, paMax;
+    paMax = ctxMain->dev.paMax;
+    // check for user-mode page table with PDPT below max physical address and not NX.
+    pte = *(PQWORD)pbPage;
+    if(((pte & 0x0000000000000087) != 0x07) || ((pte & 0x0000fffffffff000) > paMax)) { return FALSE; }
+    for(c = 0, i = 0x800; i < 0x1000; i += 8) { // minimum number of supervisor entries above 0x800
+        pte = *(PQWORD)(pbPage + i);
+        // check for user-mode page table with PDPT below max physical address and not NX.
+        if(((pte & 0x8000ff0000000087) == 0x03) && ((pte & 0x0000fffffffff000) < paMax)) { c++; }
+        // check for self-referential entry
+        if((*(PQWORD)(pbPage + i) & 0x0000fffffffff083) == pa + 0x03) { fSelfRef = TRUE; }
+    }
+    return fSelfRef && (c >= 6);
+    */
+    None
 }
 
-pub fn find_x64_pae(mem: &Vec<u8>) -> Result<DTB> {
-    Err(Error::new(ErrorKind::Other, "unable to find x64_pae dtb in lowstub < 16M"))
+pub fn find_x64(mem: &Vec<u8>) -> Result<DTB> {
+    mem.
+        chunks_exact(0x1000)
+        .position(|c| _find_x64(c).is_some())
+        .ok_or(Error::new(ErrorKind::Other, "unable to find x64 dtb in lowstub < 16M"))
+        .and_then(|i| {
+            Ok(DTB{
+                va: 0,
+                dtb: (i as u64) * 0x1000,
+            })
+        })
+}
+
+// see _find_x64
+fn _find_x86_pae(mem: &[u8]) -> Option<()> {
+    /*
+    for(QWORD i = 0; i < 0x1000; i += 8) {
+        if((i < 0x20) && ((*(PQWORD)(pbPage + i) != pa + (i << 9) + 0x1001))) {
+            return FALSE;
+        } else if((i >= 0x20) && *(PQWORD)(pbPage + i)) {
+            return FALSE;
+        }
+    }
+    return TRUE;
+    */
+    None
+}
+
+pub fn find_x86_pae(mem: &Vec<u8>) -> Result<DTB> {
+    mem.
+        chunks_exact(0x1000)
+        .position(|c| _find_x86_pae(c).is_some())
+        .ok_or(Error::new(ErrorKind::Other, "unable to find x64_pae dtb in lowstub < 16M"))
+        .and_then(|i| {
+            Ok(DTB{
+                va: 0,
+                dtb: (i as u64) * 0x1000,
+            })
+        })
+}
+
+/*
+* Check if a page looks like the Windows Kernel x86 Directory Table Base (DTB)
+* in the 32-bit mode -  i.e. the PD of the System process.
+* 1: self-referential entry exists at offset 0xC00
+* 2: PDE[0] is a user-mode PDE pointing to a PT.
+* 3: a minimum number of supervisor-mode PDEs must exist.
+*/
+fn _find_x86(mem: &[u8]) -> Option<()> {
+    /*
+    DWORD c, i;
+    if((*(PDWORD)(pbPage + 0xc00) & 0xfffff003) != pa + 0x03) { return FALSE; } // self-referential entry exists
+    if(*pbPage != 0x67) { return FALSE; }  // user-mode page table exists at 1st PTE (index 0)
+    for(c = 0, i = 0x800; i < 0x1000; i += 4) { // minimum number of supervisor entries above 0x800
+        if((*(pbPage + i) == 0x63) || (*(pbPage + i) == 0xe3)) { c++; }
+        if(c > 16) { return TRUE; }
+    }
+    return FALSE;
+    */
+    None
 }
 
 pub fn find_x86(mem: &Vec<u8>) -> Result<DTB> {
-    Err(Error::new(ErrorKind::Other, "unable to find x86 dtb in lowstub < 16M"))
+    mem.
+        chunks_exact(0x1000)
+        .position(|c| _find_x86(c).is_some())
+        .ok_or(Error::new(ErrorKind::Other, "unable to find x86 dtb in lowstub < 16M"))
+        .and_then(|i| {
+            Ok(DTB{
+                va: 0,
+                dtb: (i as u64) * 0x1000,
+            })
+        })
 }

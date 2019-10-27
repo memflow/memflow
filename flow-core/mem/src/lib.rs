75a89edddbd36b39701f62b51903c25be7e1c11f
ko1N <ko1N1337@gmail.com>
2019-10-28 00:59:26 +0100

    made virt_read_type byte order agnostic
    Former-commit-id: b67342a5732858aef9a880e5b7d70865f2ad9a2f

--- flow-core/mem/src/lib.rs
+++ flow-core/mem/src/lib.rs
@@ -1,13 +1,22 @@
 // TODO: custom error + result
 use std::io::Result;
 
-use byteorder::{ByteOrder, LittleEndian};
+use byteorder::{ByteOrder, LittleEndian, BigEndian};
 
 use address::{Address, Length};
 use arch::{self, Architecture, InstructionSet};
 
 pub trait PhysicalRead {
     fn phys_read(&mut self, addr: Address, len: Length) -> Result<Vec<u8>>;
+}
+
+macro_rules! arch_read_type {
+    ($byte_order:expr, $func:ident, $value:expr) => {
+        match $byte_order {
+            arch::ByteOrder::LittleEndian => LittleEndian::$func($value),
+            arch::ByteOrder::BigEndian => BigEndian::$func($value),
+        }
+    }
 }
 
 pub trait VirtualRead {
@@ -21,32 +30,32 @@ pub trait VirtualRead {
 
     fn virt_read_addr(&mut self, arch: Architecture, dtb: Address, addr: Address) -> Result<Address> {
         let r = self.virt_read(arch, dtb, addr, arch.instruction_set.len_addr())?;
-        Ok(Address::from(LittleEndian::read_u64(&r)))
+        Ok(Address::from(arch_read_type!(arch.instruction_set.byte_order(), read_u64, &r)))
     }
 
     fn virt_read_u64(&mut self, arch: Architecture, dtb: Address, addr: Address) -> Result<u64> {
         let r = self.virt_read(arch, dtb, addr, arch.instruction_set.len_u64())?;
-        Ok(LittleEndian::read_u64(&r))
+        Ok(arch_read_type!(arch.instruction_set.byte_order(), read_u64, &r))
     }
 
     fn virt_read_u32(&mut self, arch: Architecture, dtb: Address, addr: Address) -> Result<u32> {
         let r = self.virt_read(arch, dtb, addr, arch.instruction_set.len_u32())?;
-        Ok(LittleEndian::read_u32(&r))
+        Ok(arch_read_type!(arch.instruction_set.byte_order(), read_u32, &r))
     }
 
     fn virt_read_i64(&mut self, arch: Architecture, dtb: Address, addr: Address) -> Result<i64> {
         let r = self.virt_read(arch, dtb, addr, arch.instruction_set.len_i64())?;
-        Ok(LittleEndian::read_i64(&r))
+        Ok(arch_read_type!(arch.instruction_set.byte_order(), read_i64, &r))
     }
 
     fn virt_read_i32(&mut self, arch: Architecture, dtb: Address, addr: Address) -> Result<i32> {
         let r = self.virt_read(arch, dtb, addr, arch.instruction_set.len_i32())?;
-        Ok(LittleEndian::read_i32(&r))
+        Ok(arch_read_type!(arch.instruction_set.byte_order(), read_i32, &r))
     }
 
     fn virt_read_f32(&mut self, arch: Architecture, dtb: Address, addr: Address) -> Result<f32> {
         let r = self.virt_read(arch, dtb, addr, arch.instruction_set.len_f32())?;
-        Ok(LittleEndian::read_f32(&r))
+        Ok(arch_read_type!(arch.instruction_set.byte_order(), read_f32, &r))
     }
 }
 

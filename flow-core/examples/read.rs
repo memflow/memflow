97bebf583692a33cfb9dadb943534d3f2d475d93
ko1N <ko1N1337@gmail.com>
2019-12-09 08:56:29 +0100

    fixed test

--- flow-core/examples/read.rs
+++ flow-core/examples/read.rs
@@ -49,9 +49,9 @@ fn main() {
         if (counter % 10000) == 0 {
             let elapsed = start.elapsed().as_millis() as f64;
             if elapsed > 0.0 {
-                println!("{} reads/sec", (u64::from(counter)) / elapsed * 1000.0);
-                println!("{} reads/frame", (u64::from(counter)) / elapsed * 1000.0 / 60.0);
-                println!("{} ms/read", elapsed / (u64::from(counter)));
+                println!("{} reads/sec", (f64::from(counter)) / elapsed * 1000.0);
+                println!("{} reads/frame", (f64::from(counter)) / elapsed * 1000.0 / 60.0);
+                println!("{} ms/read", elapsed / (f64::from(counter)));
             }
         }
     }

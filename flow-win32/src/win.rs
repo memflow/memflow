e1868beb5c9918807af79cc7208634e372a83d8d
ko1N <ko1N1337@gmail.com>
2019-12-16 22:15:51 +0100

    added kernel driver iteration through PsLoadedModuleList on win32
    Former-commit-id: 3e63814b0576ba1486672dd0363b5ebfe7e9e841

--- flow-win32/src/win.rs
+++ flow-win32/src/win.rs
@@ -1,18 +1,27 @@
+use crate::error::{Error, Result};
+
+use log::info;
+
 use std::cell::RefCell;
 use std::rc::Rc;
 
-use flow_core::address::Address;
-use flow_core::mem::VirtualRead;
+use flow_core::address::{Address, Length};
+use flow_core::mem::*;
 
 use crate::kernel::StartBlock;
 
 pub mod types;
 pub mod unicode_string;
 
+pub mod kernel_process;
 pub mod module;
 pub mod process;
 
-use process::ProcessIterator;
+use goblin::pe::options::ParseOptions;
+use goblin::pe::PE;
+
+pub use kernel_process::KernelProcess;
+pub use process::{Process, ProcessIterator, ProcessTrait}; // TODO: add forward declares
 
 // TODO: cache processes somewhat?
 pub struct Windows<T: VirtualRead> {
@@ -44,6 +53,32 @@ where
 }
 
 impl<T: VirtualRead> Windows<T> {
+    pub fn kernel_process(&self) -> Result<KernelProcess<T>> {
+        let clone = self.clone();
+
+        let memory = &mut clone.mem.borrow_mut();
+        let mut reader =
+            VirtualReader::with(&mut **memory, clone.start_block.arch, self.start_block.dtb);
+        let header_buf = reader.virt_read(clone.kernel_base, Length::from_mb(32))?;
+
+        let mut pe_opts = ParseOptions::default();
+        pe_opts.resolve_rva = false;
+
+        let header = PE::parse_with_opts(&header_buf, &pe_opts).unwrap(); // TODO: error
+        let module_list = header
+            .exports
+            .iter()
+            .filter(|e| e.name.unwrap_or_default() == "PsLoadedModuleList") // PsActiveProcessHead
+            .inspect(|e| info!("found eat entry: {:?}", e))
+            .nth(0)
+            .ok_or_else(|| Error::new("unable to find export PsLoadedModuleList"))
+            .and_then(|e| Ok(clone.kernel_base + Length::from(e.rva)))?;
+
+        let addr = reader.virt_read_addr(module_list)?;
+        let rc = Rc::new(RefCell::new(self.clone()));
+        Ok(KernelProcess::with(rc, addr))
+    }
+
     pub fn process_iter(&self) -> ProcessIterator<T> {
         let rc = Rc::new(RefCell::new(self.clone()));
         ProcessIterator::new(rc)

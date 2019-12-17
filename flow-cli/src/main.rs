0491baa25bf2eecf10fa5882383f6c9c059e161f
ko1N <ko1N1337@gmail.com>
2019-12-17 01:48:36 +0100

    added a few memflow-cli commands

--- flow-cli/src/main.rs
+++ flow-cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg};
 use pretty_env_logger;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -13,19 +13,41 @@ fn main() {
     let argv = App::new("flow-core")
         .version("0.1")
         .arg(
-            Arg::with_name("bridge-url")
+            Arg::with_name("url")
                 .short("url")
-                .long("bridge-url")
+                .long("url")
                 .value_name("URL")
-                .help("bridge socket url")
+                .help("socket url")
                 .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("os")
+                .short("os")
+                .long("os")
+                .value_name("OS")
+                .help("target operating system")
+                .takes_value(true),
+        )
+        .subcommand(
+            App::new("kernel")
+                .about("os kernel specific options")
+                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand(
+                    App::new("module")
+                        .about("kernel module options")
+                        .subcommand(App::new("ls").about("lists loaded kernel modules")),
+                ),
+        )
+        .subcommand(
+            App::new("process")
+                .about("os process specific options")
+                .setting(AppSettings::SubcommandRequiredElseHelp)
+                .subcommand(App::new("ls").about("lists active processes")),
         )
         .get_matches();
 
     // this is just some test code
-    let url = argv
-        .value_of("bridge-url")
-        .unwrap_or("unix:/tmp/qemu-connector-bridge");
+    let url = argv.value_of("url").unwrap_or("unix:/tmp/memflow-bridge");
     let bridge = match BridgeClient::connect(url) {
         Ok(br) => br,
         Err(e) => {
@@ -36,15 +58,62 @@ fn main() {
 
     // os functionality should be located in core!
     let bridgerc = Rc::new(RefCell::new(bridge));
-    let win = flow_win32::init(bridgerc).unwrap();
+
+    let os = match argv.value_of("os").unwrap_or("win32") {
+        "win32" => flow_win32::init(bridgerc),
+        //"linux" => {},
+        _ => Err(flow_win32::error::Error::new("invalid os")),
+    }
+    .unwrap();
+
+    match argv.subcommand() {
+        ("kernel", Some(kernel_matches)) => match kernel_matches.subcommand() {
+            ("module", Some(module_matches)) => match module_matches.subcommand() {
+                ("ls", Some(_)) => {
+                    os.kernel_process()
+                        .unwrap()
+                        .module_iter()
+                        .unwrap()
+                        .for_each(|mut m| {
+                            if let Ok(name) = m.name() {
+                                println!("{}", name);
+                            }
+                        });
+                }
+                _ => println!("invalid command {:?}", module_matches),
+            },
+            _ => println!("invalid command {:?}", kernel_matches),
+        },
+        ("process", Some(kernel_matches)) => {
+            match kernel_matches.subcommand() {
+                ("ls", Some(_)) => {
+                    os.process_iter().for_each(|mut m| {
+                        if let Ok(pid) = m.pid() {
+                            if let Ok(name) = m.name() {
+                                println!("{} {}", pid, name);
+                            }
+                        }
+                    });
+                }
+                ("module", Some(module_matches)) => {
+                    match module_matches.subcommand() {
+                        ("ls", Some(_)) => {
+                            println!("test1") // TODO: specify process name/pid
+                        }
+                        _ => println!("invalid command {:?}", module_matches),
+                    }
+                }
+                _ => println!("invalid command {:?}", kernel_matches),
+            }
+        }
+        ("", None) => println!("no command specified"),
+        _ => println!("invalid command {:?}", argv),
+    }
 
     // parse kernel pe header -- start
-    let kernel_proc = win.kernel_process().unwrap();
-    kernel_proc
-        .module_iter()
-        .unwrap()
-        .for_each(|mut m| println!("{:?}", m.name()));
+    /*use flow_core::error::*;
 
+    */
     //println!("module_list: {:x}", module_list);
     // parse kernel pe header -- end
 
@@ -54,6 +123,7 @@ fn main() {
         win.process_iter()
             .for_each(|mut p| println!("{:?} {:?}", p.get_pid(), p.get_name()));
     */
+    /*
     let mut process = win
         .process_iter()
         .filter_map(|mut p| {
@@ -73,7 +143,6 @@ fn main() {
         process.name(),
         process.has_wow64()
     );
-    /*
         process
             .module_iter()
             .unwrap()
@@ -82,7 +151,6 @@ fn main() {
             .module_iter()
             .unwrap()
             .for_each(|mut m| println!("{:?}", m.get_name()));
-    */
     let module = process
         .module_iter()
         .unwrap()
@@ -98,4 +166,5 @@ fn main() {
         .unwrap();
 
     println!("mod: {:?}", module.clone().name());
+    */
 }

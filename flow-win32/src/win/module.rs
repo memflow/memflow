9eff50643087f1f8e880537064693ac7db5927d1
ko1N <ko1N1337@gmail.com>
2019-12-27 17:53:36 +0100

    added ModuleTrait to flow-core

--- flow-win32/src/win/module.rs
+++ flow-win32/src/win/module.rs
@@ -6,6 +6,7 @@ use crate::error::{Error, Result};
 use flow_core::address::{Address, Length};
 use flow_core::arch::{ArchitectureTrait, InstructionSet};
 use flow_core::mem::*;
+use flow_core::process::ModuleTrait;
 use flow_core::*;
 
 use std::cell::RefCell;
@@ -56,9 +57,14 @@ where
             module_name: String::from(""),
         }
     }
+}
 
+impl<T> ModuleTrait for Module<T>
+where
+    T: ProcessModuleTrait + ArchitectureTrait + VirtualReadHelperFuncs + VirtualReadUnicodeString,
+{
     // TODO: 0 should also result in an error
-    pub fn base(&mut self) -> Result<Address> {
+    fn base(&mut self) -> flow_core::Result<Address> {
         if !self.module_base.is_null() {
             return Ok(self.module_base);
         }
@@ -69,13 +75,13 @@ where
         self.module_base = match proc_arch.instruction_set {
             InstructionSet::X64 => addr!(process.virt_read_u64(self.peb_entry + len!(0x30))?), // self.get_offset("_LDR_DATA_TABLE_ENTRY", "DllBase")?
             InstructionSet::X86 => addr!(process.virt_read_u32(self.peb_entry + len!(0x18))?),
-            _ => return Err(Error::new("invalid process architecture")),
+            _ => return Err(flow_core::Error::new("invalid process architecture")),
         };
         Ok(self.module_base)
     }
 
     // TODO: 0 should also result in an error
-    pub fn size(&mut self) -> Result<Length> {
+    fn size(&mut self) -> flow_core::Result<Length> {
         if !self.module_size.is_zero() {
             return Ok(self.module_size);
         }
@@ -86,12 +92,12 @@ where
         self.module_size = match proc_arch.instruction_set {
             InstructionSet::X64 => len!(process.virt_read_u64(self.peb_entry + len!(0x40))?), // self.get_offset("_LDR_DATA_TABLE_ENTRY", "SizeOfImage")?
             InstructionSet::X86 => len!(process.virt_read_u32(self.peb_entry + len!(0x20))?),
-            _ => return Err(Error::new("invalid process architecture")),
+            _ => return Err(flow_core::Error::new("invalid process architecture")),
         };
         Ok(self.module_size)
     }
 
-    pub fn name(&mut self) -> Result<String> {
+    fn name(&mut self) -> flow_core::Result<String> {
         if self.module_name != "" {
             return Ok(self.module_name.clone());
         }
@@ -102,7 +108,7 @@ where
         let offs = match proc_arch.instruction_set {
             InstructionSet::X64 => len!(0x58), // self.get_offset("_LDR_DATA_TABLE_ENTRY", "BaseDllName")?,
             InstructionSet::X86 => len!(0x2C),
-            _ => return Err(Error::new("invalid process architecture")),
+            _ => return Err(flow_core::Error::new("invalid process architecture")),
         };
 
         // x64 = x64 && !wow64

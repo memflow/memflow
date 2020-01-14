2ec543f6c32042d1dd3f0f246dcf9a430f94bed8
ko1N <ko1N1337@gmail.com>
2020-01-14 23:10:47 +0100

    moved flow_win32::init to Win32::try_with

--- flow-win32/src/lib.rs
+++ flow-win32/src/lib.rs
@@ -1,11 +1,8 @@
-use flow_core::mem::{PhysicalRead, VirtualRead};
-use log::info;
-
 pub mod error;
-pub use error::{Error, Result};
+pub use error::*;
 
 pub mod kernel;
-pub use kernel::StartBlock;
+pub use kernel::*;
 
 //pub mod keyboard;
 //pub use keyboard::{Keyboard, KeyboardState};
@@ -17,64 +14,3 @@ pub use offsets::*;
 
 pub mod win32;
 pub use win32::*;
-
-use std::cell::RefCell;
-use std::rc::Rc;
-
-/*
-Options:
-- supply cr3 (dtb)
-- supply kernel hint
-- supply pdb
-- supply kernel offsets for basic structs (dumped windbg maybe)
-*/
-
-pub fn init<T: PhysicalRead + VirtualRead>(mem: &mut T) -> Result<Win32> {
-    // find dirtable base
-    let start_block = kernel::lowstub::find(mem)?;
-    info!(
-        "arch={:?} va={:x} dtb={:x}",
-        start_block.arch, start_block.va, start_block.dtb
-    );
-
-    // find ntoskrnl.exe base
-    let (kernel_base, kernel_size) = kernel::ntos::find(mem, &start_block)?;
-    info!("kernel_base={:x}", kernel_base);
-
-    // get ntoskrnl.exe guid
-    let kernel_guid = kernel::ntos::find_guid(mem, &start_block, kernel_base, kernel_size)?;
-    info!("kernel_guid={:?}", kernel_guid);
-
-    // system eprocess -> find
-    let eprocess_base = kernel::sysproc::find(mem, &start_block, kernel_base)?;
-    info!("eprocess_base={:x}", eprocess_base);
-
-    // get guid
-
-    // TODO: add a module like sysproc/ntoskrnl/etc which will fetch pdb with various fallbacks and return early here
-    // TODO: create fallback thingie which implements hardcoded offsets
-    // TODO: create fallback which parses C struct from conf file + manual pdb
-    // TODO: add class wrapper to Windows struct
-
-    // grab pdb
-    /*
-    let kernel_pdb = match cache::fetch_pdb_from_mem(memory, &start_block, kernel_base) {
-        Ok(p) => {
-            info!("valid kernel_pdb found: {:?}", p);
-            Some(Rc::new(RefCell::new(PDB::new(p))))
-        }
-        Err(e) => {
-            info!("unable to fetch pdb for ntoskrnl: {:?}", e);
-            None
-        }
-    };
-    */
-
-    Ok(Win32 {
-        start_block,
-        kernel_base,
-        kernel_size,
-        kernel_guid,
-        eprocess_base,
-    })
-}
--- flow-win32/src/win32/process.rs
+++ flow-win32/src/win32/process.rs
@@ -13,13 +13,10 @@ pub use win_user::*;
 
 use crate::error::Result;
 
-use super::Win32;
 use crate::offsets::Win32Offsets;
 
 use flow_core::address::Address;
-use flow_core::arch::ArchitectureTrait;
-use flow_core::mem::*;
-use flow_core::process::*;
+use flow_core::mem::VirtualRead;
 
 pub trait Win32Process {
     fn wow64(&self) -> Address;
@@ -29,67 +26,3 @@ pub trait Win32Process {
     fn peb_list<T: VirtualRead>(&self, mem: &mut T, offsets: &Win32Offsets)
         -> Result<Vec<Address>>;
 }
-
-/*
-use crate::win::module::{Module, ModuleIterator};
-
-pub trait ProcessModuleTrait {
-    fn first_peb_entry(&mut self) -> Result<Address>;
-    fn module_iter(&self) -> Result<ModuleIterator<Self>>
-    where
-        Self: Sized + ArchitectureTrait + VirtualReadHelper + VirtualReadHelperFuncs;
-}
-
-pub trait ProcessModuleHelper
-where
-    Self:
-        Sized + ProcessModuleTrait + ArchitectureTrait + VirtualReadHelper + VirtualReadHelperFuncs,
-{
-    fn first_module(&self) -> Result<Module<Self>>;
-    fn module(&self, name: &str) -> Result<Module<Self>>;
-    fn containing_module(&self, addr: Address) -> Result<Module<Self>>;
-}
-
-impl<T> ProcessModuleHelper for T
-where
-    T: Sized + ProcessModuleTrait + ArchitectureTrait + VirtualReadHelper + VirtualReadHelperFuncs,
-{
-    fn first_module(&self) -> Result<Module<Self>> {
-        Ok(self
-            .module_iter()?
-            .nth(0)
-            .ok_or_else(|| "unable to read first module")?)
-    }
-
-    fn module(&self, name: &str) -> Result<Module<Self>> {
-        Ok(self
-            .module_iter()?
-            .filter_map(|mut m| {
-                if m.name().unwrap_or_default() == name {
-                    Some(m)
-                } else {
-                    None
-                }
-            })
-            .nth(0)
-            .ok_or_else(|| "unable to find module")?)
-    }
-
-    fn containing_module(&self, addr: Address) -> Result<Module<Self>> {
-        Ok(self
-            .module_iter()?
-            .filter_map(|mut m| {
-                let base = m.base().unwrap_or_default();
-                let size = m.size().unwrap_or_default();
-
-                if base <= addr && addr <= base + size {
-                    Some(m)
-                } else {
-                    None
-                }
-            })
-            .nth(0)
-            .ok_or_else(|| "unable to find containing module")?)
-    }
-}
-*/

a1d2697cd856936f85fd3a9f39623bfdf58df7b6
ko1N <ko1N1337@gmail.com>
2020-01-25 02:16:15 +0100

    added conditional check for linux to disable qemu-procfs connector

--- flow-cli/src/init/init_qemu_procfs.rs
+++ flow-cli/src/init/init_qemu_procfs.rs
@@ -1,8 +1,15 @@
 use flow_core::*;
 
-#[cfg(feature = "connector-qemu-procfs")]
+#[cfg(all(target_os = "linux", feature = "connector-qemu-procfs"))]
 pub fn init_qemu_procfs() -> Result<flow_qemu_procfs::Memory> {
     flow_qemu_procfs::Memory::new()
+}
+
+#[cfg(not(target_os = "linux"))]
+pub fn init_qemu_procfs() -> Result<super::EmptyVirtualMemory> {
+    Err(Error::new(
+        "connector qemu-procfs is not available on this system",
+    ))
 }
 
 #[cfg(not(feature = "connector-qemu-procfs"))]

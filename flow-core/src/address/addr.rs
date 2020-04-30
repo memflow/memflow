2be8116de722291c045ab08ed60a23229bc38ecd
Aurimas Blažulionis <0x60@pm.me>
2020-04-30 16:26:29 +0100

    Make Length and Address const initializable
    Former-commit-id: be04568747b9b11312cd3ba03cb6c380739253fb

--- flow-core/src/address/addr.rs
+++ flow-core/src/address/addr.rs
@@ -44,11 +44,11 @@ impl From<Length> for Address {
 }
 
 impl Address {
-    pub fn null() -> Self {
+    pub const fn null() -> Self {
         Address { 0: 0 }
     }
 
-    pub fn is_null(self) -> bool {
+    pub const fn is_null(self) -> bool {
         self.0 == 0
     }
 
@@ -56,19 +56,19 @@ impl Address {
         Length::from(self)
     }
 
-    pub fn as_u32(self) -> u32 {
+    pub const fn as_u32(self) -> u32 {
         self.0 as u32
     }
 
-    pub fn as_u64(self) -> u64 {
+    pub const fn as_u64(self) -> u64 {
         self.0
     }
 
-    pub fn as_usize(self) -> usize {
+    pub const fn as_usize(self) -> usize {
         self.0 as usize
     }
 
-    pub fn as_page_aligned(self, page_size: Length) -> Address {
+    pub const fn as_page_aligned(self, page_size: Length) -> Address {
         Address {
             0: self.0 & (!(page_size.as_u64() - 1)),
         }

b5ddea805ab5d231564676a5ed891e9298f3751a
Aurimas Blažulionis <0x60@pm.me>
2020-05-28 00:08:40 +0100

    Add physical memory benches, make it easier to add benchmarks for other backends

--- flow-bench/benches/read_win32.rs
+++ flow-bench/benches/read_win32.rs
@@ -1,6 +1,5 @@
-
 extern crate flow_bench;
-use flow_bench::*;
+use flow_bench::{phys, virt};
 
 use criterion::*;
 
@@ -12,7 +11,7 @@ use flow_win32::{Win32, Win32Module, Win32Offsets, Win32Process};
 use rand::prelude::*;
 use rand::{prng::XorShiftRng as CurRng, Rng, SeedableRng};
 
-fn initialize_ctx() -> flow_core::Result<(Memory, Win32Process, Win32Module)> {
+fn initialize_virt_ctx() -> flow_core::Result<(Memory, Win32Process, Win32Module)> {
     let mut mem = Memory::new().unwrap();
 
     let os = Win32::try_with(&mut mem).unwrap();
@@ -57,62 +56,18 @@ fn initialize_ctx() -> flow_core::Result<(Memory, Win32Process, Win32Module)> {
     Err("No module found!".into())
 }
 
-fn read_test(
-    bench: &mut Bencher,
-    cache_size: u64,
-    chunk_size: usize,
-    chunks: usize,
-    use_tlb: bool,
-) {
-    let (mem, proc, tmod) = initialize_ctx().unwrap();
-    read_test_with_ctx(bench, cache_size, chunk_size, chunks, use_tlb, mem, proc, tmod);
-}
-
-fn win32_read_params(
-    group: &mut BenchmarkGroup<'_, measurement::WallTime>,
-    func_name: String,
-    cache_size: u64,
-    use_tlb: bool,
-    max_chunk_pow: usize
-) {
-    for i in 0..(max_chunk_pow+1) {
-        let chunk_count = 1 << i;
-        for &size in [0x8, 0x10, 0x100, 0x1000, 0x10000].iter() {
-            group.throughput(Throughput::Bytes(size * chunk_count));
-            group.bench_with_input(
-                BenchmarkId::new(format!("{}_{}_chunks", func_name, chunk_count), size),
-                &(size, chunk_count),
-                |b, &(size, chunk_count)| {
-                    read_test(
-                        b,
-                        black_box(cache_size),
-                        black_box(size as usize),
-                        black_box(chunk_count as usize),
-                        black_box(use_tlb),
-                    )
-                },
-            );
-        }
-    }
-}
-
 fn win32_read_group(c: &mut Criterion) {
-    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
-
-    let mut group = c.benchmark_group("win32_read");
-    group.plot_config(plot_config);
-
-    win32_read_params(&mut group, "win32_read_nocache".into(), 0, false, 0);
-    win32_read_params(&mut group, "win32_read_tlb_nocache".into(), 0, true, 0);
-    win32_read_params(&mut group, "win32_read_cache".into(), 2, false, 0);
-    win32_read_params(&mut group, "win32_read_tlb_cache".into(), 2, true, 0);
+    virt::seq_read(c, "win32", &initialize_virt_ctx);
+    virt::chunk_read(c, "win32", &initialize_virt_ctx);
+    phys::seq_read(c, "win32", &|| Memory::new());
+    phys::chunk_read(c, "win32", &|| Memory::new());
 }
 
 criterion_group! {
     name = win32_read;
     config = Criterion::default()
-        .warm_up_time(std::time::Duration::from_millis(500))
-        .measurement_time(std::time::Duration::from_secs(2));
+        .warm_up_time(std::time::Duration::from_millis(300))
+        .measurement_time(std::time::Duration::from_millis(2200));
     targets = win32_read_group
 }
 
---
+++ flow-bench/src/phys.rs
@@ -0,0 +1,192 @@
+use criterion::*;
+
+use flow_core::mem::{timed_validator::*, AccessPhysicalMemory, CachedMemoryAccess, PageCache};
+
+use flow_core::{Address, Architecture, Length, Page, PageType, PhysicalAddress};
+
+use rand::prelude::*;
+use rand::{prng::XorShiftRng as CurRng, Rng, SeedableRng};
+
+fn rwtest<T: AccessPhysicalMemory>(
+    mem: &mut T,
+    (start, end): (Address, Address),
+    chunk_sizes: &[usize],
+    chunk_counts: &[usize],
+    read_size: usize,
+) -> usize {
+    let mut rng = CurRng::from_rng(thread_rng()).unwrap();
+
+    let mut total_size = 0;
+
+    for i in chunk_sizes {
+        for o in chunk_counts {
+            let mut bufs = vec![(Address::null(), vec![0 as u8; *i]); *o];
+            let mut done_size = 0;
+
+            while done_size < read_size {
+                let base_addr = rng.gen_range(start.as_u64(), end.as_u64());
+
+                let _ = mem.phys_read_raw_iter(bufs.iter_mut().map(|(addr, buf)| {
+                    *addr = (base_addr + rng.gen_range(0, 0x2000)).into();
+                    (
+                        PhysicalAddress {
+                            address: *addr,
+                            page: Some(Page {
+                                page_type: PageType::from_writeable_bit(true),
+                                page_base: addr.as_page_aligned(Length::from_kb(4)),
+                                page_size: Length::from_kb(4),
+                            }),
+                        },
+                        buf.as_mut_slice(),
+                    )
+                }));
+                done_size += *i * *o;
+            }
+
+            total_size += done_size
+        }
+    }
+
+    total_size
+}
+
+pub fn read_test_with_mem<T: AccessPhysicalMemory>(
+    bench: &mut Bencher,
+    mem: &mut T,
+    chunk_size: usize,
+    chunks: usize,
+    start_end: (Address, Address),
+) {
+    bench.iter(|| {
+        black_box(rwtest(mem, start_end, &[chunk_size], &[chunks], chunk_size));
+    });
+}
+
+fn read_test_with_ctx<T: AccessPhysicalMemory>(
+    bench: &mut Bencher,
+    cache_size: u64,
+    chunk_size: usize,
+    chunks: usize,
+    mut mem: T,
+) {
+    let mut rng = CurRng::from_rng(thread_rng()).unwrap();
+
+    let start = Address::from(rng.gen_range(0, Length::from_mb(50).as_u64()));
+    let end = start + Length::from_mb(1);
+
+    if cache_size > 0 {
+        let cache = PageCache::new(
+            Architecture::X64,
+            Length::from_mb(cache_size),
+            PageType::PAGE_TABLE | PageType::READ_ONLY | PageType::WRITEABLE,
+            TimedCacheValidator::new(Duration::from_millis(10000).into()),
+        );
+
+        let mut mem = CachedMemoryAccess::with(&mut mem, cache);
+        read_test_with_mem(bench, &mut mem, chunk_size, chunks, (start, end));
+    } else {
+        read_test_with_mem(bench, &mut mem, chunk_size, chunks, (start, end));
+    }
+}
+
+fn seq_read_params<T: AccessPhysicalMemory>(
+    group: &mut BenchmarkGroup<'_, measurement::WallTime>,
+    func_name: String,
+    cache_size: u64,
+    initialize_ctx: &dyn Fn() -> flow_core::Result<T>,
+) {
+    for &size in [0x8, 0x10, 0x100, 0x1000, 0x10000].iter() {
+        group.throughput(Throughput::Bytes(size));
+        group.bench_with_input(
+            BenchmarkId::new(func_name.clone(), size),
+            &size,
+            |b, &size| {
+                read_test_with_ctx(
+                    b,
+                    black_box(cache_size),
+                    black_box(size as usize),
+                    black_box(1),
+                    initialize_ctx().unwrap(),
+                )
+            },
+        );
+    }
+}
+
+fn chunk_read_params<T: AccessPhysicalMemory>(
+    group: &mut BenchmarkGroup<'_, measurement::WallTime>,
+    func_name: String,
+    cache_size: u64,
+    initialize_ctx: &dyn Fn() -> flow_core::Result<T>,
+) {
+    for &size in [0x8, 0x10, 0x100, 0x1000].iter() {
+        for &chunk_size in [1, 4, 16, 64].iter() {
+            group.throughput(Throughput::Bytes(size * chunk_size));
+            group.bench_with_input(
+                BenchmarkId::new(format!("{}_s{:x}", func_name, size), size),
+                &size,
+                |b, &size| {
+                    read_test_with_ctx(
+                        b,
+                        black_box(cache_size),
+                        black_box(size as usize),
+                        black_box(chunk_size as usize),
+                        initialize_ctx().unwrap(),
+                    )
+                },
+            );
+        }
+    }
+}
+
+pub fn seq_read<T: AccessPhysicalMemory>(
+    c: &mut Criterion,
+    backend_name: &str,
+    initialize_ctx: &dyn Fn() -> flow_core::Result<T>,
+) {
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+
+    let group_name = format!("{}_phys_seq_read", backend_name);
+
+    let mut group = c.benchmark_group(group_name.clone());
+    group.plot_config(plot_config);
+
+    seq_read_params(
+        &mut group,
+        format!("{}_nocache", group_name),
+        0,
+        initialize_ctx,
+    );
+    seq_read_params(
+        &mut group,
+        format!("{}_cache", group_name),
+        2,
+        initialize_ctx,
+    );
+}
+
+pub fn chunk_read<T: AccessPhysicalMemory>(
+    c: &mut Criterion,
+    backend_name: &str,
+    initialize_ctx: &dyn Fn() -> flow_core::Result<T>,
+) {
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+
+    let group_name = format!("{}_phys_chunk_read", backend_name);
+
+    let mut group = c.benchmark_group(group_name.clone());
+    group.plot_config(plot_config);
+
+    chunk_read_params(
+        &mut group,
+        format!("{}_nocache", group_name),
+        0,
+        initialize_ctx,
+    );
+    chunk_read_params(
+        &mut group,
+        format!("{}_cache", group_name),
+        2,
+        initialize_ctx,
+    );
+}

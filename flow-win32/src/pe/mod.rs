06b1c14905afae7e6d90dbb7aa7d70977002a7f6
ko1N <ko1N1337@gmail.com>
2020-06-06 22:11:14 +0200

    Removed unnecessary Wrap enum for Lazy Pe View; Added get_section_bytes implementation

--- flow-win32/src/pe/mod.rs
+++ flow-win32/src/pe/mod.rs
@@ -8,17 +8,11 @@ use pelite::{Error, PeView, Result};
 use flow_core::mem::VirtualMemory;
 use flow_core::types::{Address, Length};
 
-enum PeFormat {
-    PE32,
-    PE64,
-}
-
 /// Wrapping Context to enable the MemoryPeView to be Copy-able
 pub struct MemoryPeViewContext<T: VirtualMemory> {
     virt_mem: RefCell<T>,
     image_base: Address,
-    format: PeFormat,
-    image_cache: UnsafeCell<Vec<u8>>,
+    image_cache: UnsafeCell<Box<[u8]>>,
 }
 
 impl<T: VirtualMemory> MemoryPeViewContext<T> {
@@ -31,20 +25,17 @@ impl<T: VirtualMemory> MemoryPeViewContext<T> {
 
         let view = PeView::from_bytes(image_header.as_ref())?;
 
+        // TODO: check if size_of_image < 0x1000 or too huge / or use SizeOfHeaders
         let size_of_image = match view.optional_header() {
             pelite::Wrap::T32(opt32) => opt32.SizeOfImage,
             pelite::Wrap::T64(opt64) => opt64.SizeOfImage,
         };
-        let mut image_cache = vec![0u8; size_of_image as usize];
+        let mut image_cache = vec![0u8; size_of_image as usize].into_boxed_slice();
         image_cache[..image_header.len()].copy_from_slice(&image_header);
 
         Ok(Self {
             virt_mem: RefCell::new(virt_mem),
             image_base,
-            format: match view {
-                pelite::Wrap::T32(_) => PeFormat::PE32,
-                pelite::Wrap::T64(_) => PeFormat::PE64,
-            },
             image_cache: UnsafeCell::new(image_cache),
         })
     }
@@ -62,74 +53,5 @@ impl<T: VirtualMemory> MemoryPeViewContext<T> {
                 &mut (*self.image_cache.get())[addr.as_usize()..(addr + len).as_usize()],
             )
             .ok();
-    }
-}
-
-//----------------------------------------------------------------
-
-/// Wraps 32-bit and 64-bit variants.
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(::serde::Serialize), serde(untagged))]
-pub enum Wrap<T32, T64> {
-    T32(T32),
-    T64(T64),
-}
-
-impl<Iter32: Iterator, Iter64: Iterator> Iterator for Wrap<Iter32, Iter64> {
-    type Item = Wrap<Iter32::Item, Iter64::Item>;
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            Wrap::T32(iter32) => iter32.next().map(Wrap::T32),
-            Wrap::T64(iter64) => iter64.next().map(Wrap::T64),
-        }
-    }
-}
-
-impl<T32, T64> Wrap<Result<T32>, Result<T64>> {
-    /// Transposes a wrap of results in a result of a wrap.
-    #[inline]
-    pub fn transpose(self) -> Result<Wrap<T32, T64>> {
-        match self {
-            Wrap::T32(Ok(ok)) => Ok(Wrap::T32(ok)),
-            Wrap::T32(Err(err)) => Err(err),
-            Wrap::T64(Ok(ok)) => Ok(Wrap::T64(ok)),
-            Wrap::T64(Err(err)) => Err(err),
-        }
-    }
-}
-impl<T32, T64> Wrap<Option<T32>, Option<T64>> {
-    /// Transposes a wrap of options in an option of a wrap.
-    #[inline]
-    pub fn transpose(self) -> Option<Wrap<T32, T64>> {
-        match self {
-            Wrap::T32(Some(some)) => Some(Wrap::T32(some)),
-            Wrap::T32(None) => None,
-            Wrap::T64(Some(some)) => Some(Wrap::T64(some)),
-            Wrap::T64(None) => None,
-        }
-    }
-}
-impl<T> Wrap<T, T> {
-    /// Unwraps the wrapped value of equal types.
-    #[inline]
-    pub fn into(self) -> T {
-        match self {
-            Wrap::T32(val) => val,
-            Wrap::T64(val) => val,
-        }
-    }
-}
-
-/// Format agnostic Lazy PE view.
-pub type MemoryPeView<'a, T> =
-    Wrap<pe32::MemoryPeView<'a, T>, pe64::MemoryPeView<'a, T>>;
-
-impl<'a, T: VirtualMemory> MemoryPeView<'a, T> {
-    pub fn new(context: &'a MemoryPeViewContext<T>) -> Result<MemoryPeView<'a, T>> {
-        match context.format {
-            PeFormat::PE64 => Ok(Wrap::T64(pe64::MemoryPeView::new(context)?)),
-            PeFormat::PE32 => Ok(Wrap::T32(pe32::MemoryPeView::new(context)?)),
-        }
     }
 }

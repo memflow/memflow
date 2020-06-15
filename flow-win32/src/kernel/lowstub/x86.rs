bcd9a2868a188ec1572f7b5a0acbb728bf207bc7
Aurimas Blažulionis <0x60@pm.me>
2020-06-15 11:03:13 +0100

    x86 (non-pae) lowstub
    Former-commit-id: cc53950f1aeb4edb665e1d1e50a24699e331f262

--- flow-win32/src/kernel/lowstub/x86.rs
+++ flow-win32/src/kernel/lowstub/x86.rs
@@ -2,40 +2,34 @@ use crate::error::{Error, Result};
 use crate::kernel::StartBlock;
 
 use byteorder::{ByteOrder, LittleEndian};
-
 use flow_core::architecture::{self, Architecture};
+use flow_core::iter::PageChunks;
 use flow_core::types::Address;
 
-fn _find(base: Address, mem: &[u8]) -> Option<()> {
-    /*
-    DWORD c, i;
-    if((*(PDWORD)(pbPage + 0xc00) & 0xfffff003) != pa + 0x03) { return FALSE; } // self-referential entry exists
-    if(*pbPage != 0x67) { return FALSE; }  // user-mode page table exists at 1st PTE (index 0)
-    for(c = 0, i = 0x800; i < 0x1000; i += 4) { // minimum number of supervisor entries above 0x800
-        if((*(pbPage + i) == 0x63) || (*(pbPage + i) == 0xe3)) { c++; }
-        if(c > 16) { return TRUE; }
+fn check_page(base: Address, mem: &[u8]) -> bool {
+    if mem[0] != 0x67 {
+        return false;
     }
-    return FALSE;
-    */
 
-    if (LittleEndian::read_u32(&mem[0xC00..]) & 0xfffff003) != (base.as_u32() + 0x3) {
-        return None;
+    if (LittleEndian::read_u32(&mem[0xc00..]) & 0xfffff003) != (base.as_u32() + 0x3) {
+        return false;
     }
-    println!("first check passed");
 
-    None
+    match mem
+        .iter()
+        .step_by(4)
+        .skip(0x200)
+        .filter(|&&x| x == 0x63 || x == 0xe3)
+        .count()
+    {
+        x if x > 16 => true,
+        _ => false,
+    }
 }
 
 pub fn find(mem: &[u8]) -> Result<StartBlock> {
-    mem.chunks_exact(architecture::x86::page_size().as_usize())
-        .enumerate()
-        .map(|(i, c)| {
-            (
-                Address::from(architecture::x86::page_size().as_u64() * i as u64),
-                c,
-            )
-        })
-        .find(|(a, c)| _find(a.clone(), c).is_some())
+    mem.page_chunks(Address::from(0), architecture::x86::page_size())
+        .find(|(a, c)| check_page(a.clone(), c))
         .ok_or_else(|| Error::new("unable to find x86 dtb in lowstub < 16M"))
         .and_then(|(a, _)| {
             Ok(StartBlock {

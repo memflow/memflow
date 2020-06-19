22f1afa391ef84a04f75e7aec26fac30acd9324d
ko1N <ko1N1337@gmail.com>
2020-06-19 15:22:07 +0200

    Implemented builder pattern for KernelInfo initialization
    Former-commit-id: d591efd69533c4fa6cc5509ffbeec8b8994a1c6e

--- flow-win32/examples/read_bench.rs
+++ flow-win32/examples/read_bench.rs
@@ -138,7 +138,7 @@ fn read_bench<T: PhysicalMemory, V: VirtualTranslate>(
 
 fn main() -> flow_core::Result<()> {
     let mut mem_sys = Memory::new()?;
-    let kernel_info = KernelInfo::find(&mut mem_sys)?;
+    let kernel_info = KernelInfo::builder().mem(&mut mem_sys).build()?;
 
     let mut vat = TranslateArch::new(kernel_info.start_block.arch);
 

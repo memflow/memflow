79b7242e734952416f5fee383061fb43ed8c8dd5
ko1N <ko1N1337@gmail.com>
2020-06-27 17:49:35 +0200

    Added documentation to mem_map

--- flow-core/src/mem/mem_map.rs
+++ flow-core/src/mem/mem_map.rs
@@ -5,6 +5,24 @@ use std::default::Default;
 use std::fmt;
 use std::prelude::v1::*;
 
+/// The `MemoryMap`struct provides a mechanism to map addresses from the linear address space
+/// that memflow uses internally to hardware specific memory regions.
+///
+/// All memory addresses will be bounds checked.
+///
+/// # Examples
+///
+/// ```
+/// use flow_core::mem::MemoryMap;
+///
+/// let mut map = MemoryMap::new();
+/// map.push(0x1000.into(), 0x1000, 0.into());      // push region from 0x1000 - 0x1FFF
+/// map.push(0x3000.into(), 0x1000, 0x2000.into()); // push region from 0x3000 - 0x3FFFF
+///
+/// println!("{:?}", map);
+///
+/// let hw_addr = map.map(0x10ff.into());
+/// ```
 #[derive(Clone)]
 pub struct MemoryMap {
     mappings: Vec<MemoryMapping>,
@@ -26,11 +44,13 @@ impl Default for MemoryMap {
 }
 
 impl MemoryMap {
+    /// Constructs a new memory map.
+    /// This function is identical to `MemoryMap::default()`.
     pub fn new() -> Self {
         MemoryMap::default()
     }
 
-    /// Adds a new memory mapping to this memory map
+    /// Adds a new memory mapping to this memory map.
     pub fn push(&mut self, base: Address, size: usize, real_base: Address) {
         // TODO: sort by base
         self.mappings.push(MemoryMapping {
@@ -41,7 +61,7 @@ impl MemoryMap {
     }
 
     /// Maps a linear address to a hardware address.
-    /// Returns an `Error::Bounds` error if the address does not lie within any memory region.
+    /// Returns `Error::Bounds` if the address is not contained within any memory region.
     pub fn map(&mut self, addr: Address) -> Result<Address> {
         let mapping = self
             .mappings
@@ -86,12 +106,28 @@ mod tests {
         map.push(0x1000.into(), 0x1000, 0.into());
         map.push(0x3000.into(), 0x1000, 0x2000.into());
 
-        assert_eq!(map.map(0x00ff.into()).is_err(), true);
         assert_eq!(map.map(0x10ff.into()), Ok(Address::from(0x00ff)));
-        assert_eq!(map.map(0x20ff.into()).is_err(), true);
         assert_eq!(map.map(0x30ff.into()), Ok(Address::from(0x20ff)));
+    }
+
+    #[test]
+    fn test_mapping_edges() {
+        let mut map = MemoryMap::new();
+        map.push(0x1000.into(), 0x1000, 0.into());
+        map.push(0x3000.into(), 0x1000, 0x2000.into());
+
         assert_eq!(map.map(0x3000.into()), Ok(Address::from(0x2000)));
         assert_eq!(map.map(0x3fff.into()), Ok(Address::from(0x2fff)));
+    }
+
+    #[test]
+    fn test_mapping_out_of_bounds() {
+        let mut map = MemoryMap::new();
+        map.push(0x1000.into(), 0x1000, 0.into());
+        map.push(0x3000.into(), 0x1000, 0x2000.into());
+
+        assert_eq!(map.map(0x00ff.into()).is_err(), true);
+        assert_eq!(map.map(0x20ff.into()).is_err(), true);
         assert_eq!(map.map(0x4000.into()).is_err(), true);
         assert_eq!(map.map(0x40ff.into()).is_err(), true);
     }

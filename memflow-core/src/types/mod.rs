1c22a16d12b42b83a2d57d25e1fff9f99867e6ec
ko1N <ko1N1337@gmail.com>
2020-07-26 14:15:25 +0200

    Cleaned up and documented memflow_core/process

--- memflow-core/src/types/mod.rs
+++ memflow-core/src/types/mod.rs
@@ -7,21 +7,27 @@ it exposes different size helpers.
 */
 
 pub mod address;
+#[doc(hidden)]
 pub use address::Address;
 
 pub mod size;
 
 pub mod page;
+#[doc(hidden)]
 pub use page::{Page, PageType};
 
 pub mod physical_address;
+#[doc(hidden)]
 pub use physical_address::PhysicalAddress;
 
 pub mod pointer32;
+#[doc(hidden)]
 pub use pointer32::Pointer32;
 
 pub mod pointer64;
+#[doc(hidden)]
 pub use pointer64::Pointer64;
 
-pub mod bswap;
-pub use bswap::ByteSwap;
+pub mod byte_swap;
+#[doc(hidden)]
+pub use byte_swap::ByteSwap;

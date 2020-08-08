1921a1dea0170d8b2e90c044e918a187f2acd6ca
ko1N <ko1N1337@gmail.com>
2020-08-08 14:38:14 +0200

    Added keystate documentation
    Former-commit-id: e358f6fbc744b0767493ef0808fdb7a956ca8442

--- memflow-win32/src/lib.rs
+++ memflow-win32/src/lib.rs
@@ -7,13 +7,17 @@ It is used to interface with windows targets.
 extern crate no_std_compat as std;
 
 pub mod error;
+#[doc(hidden)]
 pub use error::*;
 
 // TODO: private these
 pub mod kernel;
+#[doc(hidden)]
 pub use kernel::*;
 
+// TODO: feature gate pelite + maybe add goblin
 pub mod pe;
+#[doc(hidden)]
 pub use pe::*; // TODO: restrict forwarding
 
 // TODO: enable again
@@ -21,7 +25,9 @@ pub use pe::*; // TODO: restrict forwarding
 //pub use keyboard::*;
 
 pub mod offsets;
+#[doc(hidden)]
 pub use offsets::*;
 
 pub mod win32;
+#[doc(hidden)]
 pub use win32::*;

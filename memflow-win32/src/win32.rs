4b940fc77f746e5cae75307b3eb6756095570352
ko1N <ko1N1337@gmail.com>
2020-08-16 14:16:00 +0200

    Renamed keystate to keyboard

--- memflow-win32/src/win32.rs
+++ memflow-win32/src/win32.rs
@@ -4,12 +4,12 @@ pub mod kernel_info;
 pub use kernel::{Kernel, KernelBuilder};
 pub use kernel_info::KernelInfo;
 
-pub mod keystate;
+pub mod keyboard;
 pub mod module;
 pub mod process;
 pub mod unicode_string;
 
-pub use keystate::*;
+pub use keyboard::*;
 pub use module::*;
 pub use process::*;
 pub use unicode_string::*;

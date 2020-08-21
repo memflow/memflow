ce97a583422a37371865ce9447fec7fb2e872d3d
Aurimas Blažulionis <0x60@pm.me>
2020-08-21 16:04:35 +0300

    Make Win32Process cloneable again, implement Clone for Box<dyn CloneableVirtualMemory>
    Former-commit-id: c29a4e45d9d0d111b847cd94a813649b094459e9

--- memflow-win32/src/win32/ext.rs
+++ memflow-win32/src/win32/ext.rs
@@ -1,9 +1,28 @@
 use memflow_core::architecture::x86;
 use memflow_core::architecture::{AddressTranslator, Architecture};
-use memflow_core::mem::{PhysicalMemory, VirtualFromPhysical, VirtualMemory, VirtualTranslate};
+use memflow_core::mem::{
+    CloneableVirtualMemory, PhysicalMemory, VirtualFromPhysical, VirtualMemory, VirtualTranslate,
+};
 use memflow_core::types::Address;
 use std::ptr;
 
+pub fn make_virt_mem_clone<
+    T: PhysicalMemory + Clone + 'static,
+    V: VirtualTranslate + Clone + 'static,
+>(
+    mem: T,
+    vat: V,
+    proc_arch: &'static dyn Architecture,
+    sys_arch: &'static dyn Architecture,
+    dtb: Address,
+) -> Box<dyn CloneableVirtualMemory> {
+    Box::new(VirtualFromPhysical::with_vat(
+        mem,
+        proc_arch,
+        x86::new_translator(dtb, sys_arch).unwrap(),
+        vat,
+    ))
+}
 pub fn make_virt_mem<'a, T: PhysicalMemory + 'a, V: VirtualTranslate + 'a>(
     mem: T,
     vat: V,

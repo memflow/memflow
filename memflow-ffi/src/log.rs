58491cdeb223d424fb16c11fcb0be28145557de9
ko1N <ko1N1337@gmail.com>
2020-08-27 13:20:24 +0200

    Added connector inventory to ffi
    Former-commit-id: 068e305550e338f1bacb3d4b18b023e7a6ff8d32

--- memflow-ffi/src/log.rs
+++ memflow-ffi/src/log.rs
@@ -3,10 +3,11 @@ use log::Level;
 #[no_mangle]
 pub extern "C" fn log_init(level: i32) {
     match level {
+        0 => simple_logger::init_with_level(Level::Error).unwrap(),
         1 => simple_logger::init_with_level(Level::Warn).unwrap(),
         2 => simple_logger::init_with_level(Level::Info).unwrap(),
         3 => simple_logger::init_with_level(Level::Debug).unwrap(),
         4 => simple_logger::init_with_level(Level::Trace).unwrap(),
-        _ => simple_logger::init_with_level(Level::Error).unwrap(),
+        _ => simple_logger::init_with_level(Level::Trace).unwrap(),
     }
 }

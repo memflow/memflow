e47c03927838ee135ad2a18aca6b209f3c120db5
Aurimas Blažulionis <0x60@pm.me>
2020-09-01 11:56:54 +0300

    Add back the runner script, this time checking for qemu_procfs connector

---
+++ runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ "$@" =~ "qemu_procfs" ]]; then
+	CWD=$(pwd)
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+	cd $DIR
+	bash setperms.sh
+	cd $CWD
+fi
+
+echo $(env)
+
+exec $@

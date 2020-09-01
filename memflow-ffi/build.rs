6ea40452d2a17b1463523b0927f44356cb9e3782
ko1N <ko1N1337@gmail.com>
2020-09-01 23:02:13 +0200

    Moved cbindgen generated header to target folder

--- memflow-ffi/build.rs
+++ memflow-ffi/build.rs
@@ -1,13 +1,10 @@
 extern crate cbindgen;
 
 use std::env;
-use std::fs;
 use std::path::PathBuf;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-
     const HEADER_NAME: &str = "memflow.h";
 
     let config = cbindgen::Config::from_root_or_default(&crate_dir);
@@ -20,11 +17,5 @@ fn main() {
         .with_config(config)
         .generate()
         .expect("Unable to generate bindings")
-        .write_to_file(out_path.join(HEADER_NAME));
-
-    fs::copy(
-        out_path.join(HEADER_NAME),
-        PathBuf::from(crate_dir).join(HEADER_NAME),
-    )
-    .unwrap();
+        .write_to_file(PathBuf::from(crate_dir).join("target").join(HEADER_NAME));
 }

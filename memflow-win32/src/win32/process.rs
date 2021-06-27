f6d511b6435e37d409e28cd0e162e3ad4bfa16aa
chiddy <dmw14w@gmail.com>
2021-06-27 15:32:04 +1000

    fix warning

--- memflow-win32/src/win32/process.rs
+++ memflow-win32/src/win32/process.rs
@@ -258,7 +258,7 @@ impl<T: VirtualMemory> Process for Win32Process<T> {
             }
             .unwrap();
 
-            if let Ok((addr, true)) = info
+            if let Ok((_, true)) = info
                 .module_base_from_entry(address, &mut s.virt_mem, arch)
                 .map(|b| (b, b == s.proc_info.section_base))
             {

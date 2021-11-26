69e6c2f03f6e9b6092befaf816016819347b1239
Aurimas Blažulionis <0x60@pm.me>
2021-11-26 17:15:01 +0000

    Add patch section

---
+++ memflow/src/types/mem.rs
@@ -0,0 +1,7 @@
+//! Common type aliases for cross-platform introspection
+
+/// Unsigned memory type. This type is guaranteed to hold a whole address of introspection target.
+pub type umem = u64;
+
+/// Signed memory type. This type is useful for address differences.
+pub type imem = i64;

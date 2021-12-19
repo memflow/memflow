f47cabc0dd38d02efce15a58356f52e3f7bd762d
ko1N <ko1N1337@gmail.com>
2021-12-19 16:37:46 +0100

    Fixed nostd builds

--- nostd-test/src/main.rs
+++ nostd-test/src/main.rs
@@ -15,8 +15,8 @@ use log::*;
 use uefi::{Handle, Status};
 
 #[entry]
-fn efi_main(_handle: Handle, st: SystemTable<Boot>) -> Status {
-    uefi_services::init(&st).expect_success("Failed to initialize utilities");
+fn efi_main(_handle: Handle, mut st: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut st).expect_success("Failed to initialize utilities");
 
     info!("memflow EFI test");
 

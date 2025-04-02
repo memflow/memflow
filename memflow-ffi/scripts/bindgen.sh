2b1516ba91c766a762b83f79f5ff29e150d226c7
ko1N <ko1N1337@gmail.com>
2025-04-02 20:07:04 +0200

    Update code coverage action

--- memflow-ffi/scripts/bindgen.sh
+++ memflow-ffi/scripts/bindgen.sh
@@ -19,5 +19,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 "$SCRIPT_DIR/bindgen_prereq.sh"
 
 # generate c and cpp bindings
-run_twice rustup run nightly cglue-bindgen +nightly -c cglue.toml -- --config cbindgen.toml --crate memflow-ffi --output memflow.h -l C
-run_twice rustup run nightly cglue-bindgen +nightly -c cglue.toml -- --config cbindgen.toml --crate memflow-ffi --output memflow.hpp -l C++
+run_twice rustup run stable cglue-bindgen +nightly -c cglue.toml -- --config cbindgen.toml --crate memflow-ffi --output memflow.h -l C
+run_twice rustup run stable cglue-bindgen +nightly -c cglue.toml -- --config cbindgen.toml --crate memflow-ffi --output memflow.hpp -l C++
